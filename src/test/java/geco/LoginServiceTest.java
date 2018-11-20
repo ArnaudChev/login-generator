77bdf3ae5d53b3969efa2c14c61a1041f043cb09
Arnaud <arnaudchevallier@live.fr>
2018-11-20 12:48:26 +0100

    #1 Create test for Login service

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -3,27 +3,46 @@ package geco;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class LoginServiceTest {
+    private  LoginService loginService;
 
     @Before
     public void setUp() throws Exception {
+        String[] tmp = new String[3];
+        tmp[0] = "test";
+        tmp[1] = "truc";
+        tmp[2] = "foo";
+        loginService = new LoginService(tmp);
     }
 
     @Test
-    public void loginExists() {
+    public void testLoginExists() {
+        assertTrue("erreur",loginService.loginExists("test"));
     }
 
     @Test
-    public void addLogin() {
+    public void testAddLogin() {
+        loginService.addLogin("bidule");
+        assertTrue("login introuvable",loginService.loginExists("bidule"));
     }
 
     @Test
     public void findAllLoginsStartingWith() {
+        assertTrue(loginService.findAllLoginsStartingWith("tru").contains("truc"));
     }
 
     @Test
     public void findAllLogins() {
+        List<String> tmp;
+        tmp = loginService.findAllLogins();
+        assertTrue(tmp.contains("test"));
+        assertTrue(tmp.contains("truc"));
+        assertTrue(tmp.contains("foo"));
+
     }
 }
